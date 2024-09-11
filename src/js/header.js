a019582bb4af040186c343302844198eba2ce237
JungHyun Lee <leeu4728@gmail.com>
2024-09-11 17:09:35 +0900

    scss 추가, js 분리, 테스트 이미지 추가

---
+++ src/js/header.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = $("#header");
+  const headerMenu = $("#header-menu");
+  const menuBtn = $("#menu-btn");
+  let allMenuFlag = false;
+
+  header
+    .find(".menu-item")
+    .off()
+    .on("mouseenter mouseleave", function (e) {
+      let type = e.type;
+      switch (type) {
+        case "mouseenter":
+          header.addClass("on");
+
+          break;
+        case "mouseleave":
+          header.removeClass("on");
+          break;
+        default:
+          break;
+      }
+    });
+
+  menuBtn.off().on("click", function () {
+    menuBtn.toggleClass("on");
+    header.toggleClass("menuOn");
+    headerMenu.toggleClass("on");
+    allMenuFlag = !allMenuFlag;
+    $("body").toggleClass("hidden");
+    if (allMenuFlag) {
+    } else {
+    }
+  });
+});
