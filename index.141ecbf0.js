50d2562a0b40efe8424c76fb37127f0d7203a5fe
JungHyun Lee <leeu4728@gmail.com>
2024-11-11 14:38:56 +0900

    Updates

---
+++ index.141ecbf0.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    AOS.init();
+    setTimeout(()=>{
+        if ($(".page-title")) $(".page-title .title").addClass("on");
+    }, 600);
+    let popupStatus = sessionStorage.getItem("popup-list");
+    if (popupStatus) popupStatus = JSON.parse(popupStatus);
+    else popupStatus = [];
+    const popupList = $(".popup-list .popup-box");
+    for (const popup of popupList){
+        const index = $(popup).data("index");
+        if (popupStatus.indexOf(index) !== -1) $(popup).hide();
+        else $(popup).show();
+    }
+    $(".popup-list .popup-box .close-btn").off("click").on("click", function() {
+        $(this).closest(".popup-box").hide();
+    });
+    $(".popup-list .popup-box .checkbox").off("click").on("click", function() {
+        $(this).closest(".popup-box").hide();
+        popupStatus.push($(this).closest(".popup-box").data("index"));
+        sessionStorage.setItem("popup-list", JSON.stringify(popupStatus));
+    });
+});
+
+//# sourceMappingURL=index.141ecbf0.js.map
